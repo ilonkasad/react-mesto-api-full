ef7cb417796da8abdababd4c2f8fbdd0a5a530c4
Ilona Akhmadullina <ilonkasad@yandex.ru>
2021-04-18 16:11:55 +0300

    Проектная работа 15 v11

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -50,6 +50,8 @@ const getProfile = (req, res, next) => {
     .catch(next);
 };
 const getMeProfile = (req, res, next) => {
+  console.log(req);
+  console.log(req.user._id);
   User.findById(req.user._id)
     .then((user) => {
       if (user === null) {
